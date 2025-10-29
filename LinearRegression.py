ce6d768093c3123b956912504715ac778211b38a
urwisher <vedantgpadwal415@gmail.com>
2025-10-29 23:41:51 +0530

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,13 +1,28 @@
+import numpy as np
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import make_regression
-import matplotlib.pyplot as plt
 
-X, y = make_regression(n_samples=100, n_features=1, noise=10)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-model = LinearRegression().fit(X_train, y_train)
+n = int(input("Enter number of data points: "))
+x = []
+y = []
 
-plt.scatter(X_test, y_test, color='blue')
-plt.plot(X_test, model.predict(X_test), color='red')
-plt.title("Linear Regression")
-plt.show()
+for i in range(n):
+    xi = float(input(f"Enter X{i+1}: "))
+    yi = float(input(f"Enter Y{i+1}: "))
+    x.append([xi])
+    y.append(yi)
+
+x = np.array(x)
+y = np.array(y)
+
+model = LinearRegression()
+model.fit(x, y)
+
+print("\nCoefficient (Slope):", model.coef_[0])
+print("Intercept:", model.intercept_)
+
+pred = float(input("\nEnter X value to predict Y: "))
+print("Predicted Y =", model.predict([[pred]])[0])
+
+y_pred = model.predict(x)
+for i in range(n):
+    print(f"X={x[i][0]}, Actual Y={y[i]}, Predicted Y={round(y_pred[i],2)}")
