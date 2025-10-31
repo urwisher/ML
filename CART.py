c9cd8632897e1794c40f61a772e4b4967f7f41a0
urwisher <vedantgpadwal415@gmail.com>
2025-10-31 20:09:17 +0530

    Update CART.py

--- CART.py
+++ CART.py
@@ -3,18 +3,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.datasets import load_iris
 
-# Load dataset
 iris = load_iris()
 X, y = iris.data, iris.target
 
-# Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=45)
 
-# Train CART model
 model = DecisionTreeClassifier(criterion='gini', random_state=45)
 model.fit(X_train, y_train)
 
-# Predict and evaluate
 y_pred = model.predict(X_test)
 acc = accuracy_score(y_test, y_pred)
 
